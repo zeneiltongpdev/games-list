25dd1381068fa3706cf4f7d12f5489acff004989
✞Ȥɘɳɘʅ✞ <zeneiltongranja@gmail.com>
2023-06-13 13:38:43 -0300

    createGameMinDTO

---
+++ back-end/dslist/src/main/java/com/javadslist/dslist/dto/GameMinDTO.java
@@ -0,0 +1,48 @@
+package com.javadslist.dslist.dto;
+
+import com.javadslist.dslist.entities.Game;
+
+public class GameMinDTO {
+    
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO() {
+    }
+
+    public GameMinDTO(Game entity) {
+       id = entity.getId();
+       title = entity.getTitle();
+       year = entity.getYear();
+       imgUrl = entity.getImgUrl();
+       shortDescription = entity.getShortDescription();
+    
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    
+    
+
+}
